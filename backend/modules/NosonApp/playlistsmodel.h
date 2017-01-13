ba583250256186e3d6b8609664e51d6b9336d5a1
janbar <jlbarriere68@gmail.com>
2017-01-13 23:15:26 +0100

    upgrade license GPLv3.0

--- backend/modules/NosonApp/playlistsmodel.h
+++ backend/modules/NosonApp/playlistsmodel.h
@@ -1,21 +1,20 @@
 /*
  *      Copyright (C) 2015-2016 Jean-Luc Barriere
  *
- *  This library is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published
- *  by the Free Software Foundation; either version 3, or (at your option)
- *  any later version.
+ *  This file is part of Noson-App
  *
- *  This library is distributed in the hope that it will be useful,
+ *  Noson-App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Noson is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU Lesser General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this library; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
- *  MA 02110-1301 USA
- *  http://www.gnu.org/copyleft/gpl.html
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
