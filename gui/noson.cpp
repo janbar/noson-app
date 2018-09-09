40c941ba2525d889d54b8eda8dd9960c26c2fdb0
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2018-09-09 12:40:46 +0200

    just link statically against nosonapp as well

--- gui/noson.cpp
+++ gui/noson.cpp
@@ -17,6 +17,12 @@
 #include <NosonThumbnailer/albumartgenerator.h>
 #include <NosonThumbnailer/artistartgenerator.h>
 
+#include <NosonApp/sonos.h>
+#include <NosonApp/player.h>
+#include <NosonApp/renderingmodel.h>
+#include <NosonApp/alarmsmodel.h>
+#include <NosonApp/qmlsortfiltermodel.h>
+
 #define CACHE_SIZE 100000000L
 
 #if (defined(_WIN32) || defined(_WIN64))
@@ -113,6 +119,43 @@ int main(int argc, char *argv[])
     engine.rootContext()->setContextProperty("Thumbnailer", new thumbnailer::qml::Proxy(thumbnailer));
     engine.addImageProvider("albumart", new thumbnailer::qml::AlbumArtGenerator(thumbnailer));
     engine.addImageProvider("artistart", new thumbnailer::qml::ArtistArtGenerator(thumbnailer));
+
+    // register utils types
+    qmlRegisterType<QSortFilterProxyModelQML>("NosonApp", 1, 0, "SortFilterModel");
+    qmlRegisterUncreatableType<FilterBehavior>("NosonApp", 1, 1, "FilterBehavior", "Not instantiable");
+    qmlRegisterUncreatableType<SortBehavior>("NosonApp", 1, 1, "SortBehavior", "Not instantiable");
+
+    // register noson singletons
+    qmlRegisterSingletonType<Sonos>("NosonApp", 1, 0, "Sonos", Sonos::sonos_provider);
+    qmlRegisterSingletonType<ZonesModel>("NosonApp", 1, 0, "AllZonesModel", Sonos::allZonesModel_provider);
+    qmlRegisterSingletonType<AlbumsModel>("NosonApp", 1, 0, "AllAlbumsModel", Sonos::allAlbumsModel_provider);
+    qmlRegisterSingletonType<ArtistsModel>("NosonApp", 1, 0, "AllArtistsModel", Sonos::allArtistsModel_provider);
+    qmlRegisterSingletonType<ComposersModel>("NosonApp", 1, 0, "AllComposersModel", Sonos::allComposersModel_provider);
+    qmlRegisterSingletonType<GenresModel>("NosonApp", 1, 0, "AllGenresModel", Sonos::allGenresModel_provider);
+    qmlRegisterSingletonType<PlaylistsModel>("NosonApp", 1, 0, "AllPlaylistsModel", Sonos::allPlaylistsModel_provider);
+    qmlRegisterSingletonType<FavoritesModel>("NosonApp", 1, 0, "AllFavoritesModel", Sonos::allFavoritesModel_provider);
+    qmlRegisterSingletonType<ServicesModel>("NosonApp", 1, 0, "MyServicesModel", Sonos::MyServicesModel_provider);
+    qmlRegisterSingletonType<AllServicesModel>("NosonApp", 1, 0, "AllServicesModel", Sonos::allServicesModel_provider);
+    //qmlRegisterSingletonType<TracksModel>("NosonApp", 1, 0, "AllTracksModel", Sonos::allTracksModel_provider);
+
+    // register noson instantiable types
+    qmlRegisterType<Player>("NosonApp", 1, 0, "ZonePlayer");
+    qmlRegisterType<ZonesModel>("NosonApp", 1, 0, "ZonesModel");
+    qmlRegisterType<RoomsModel>("NosonApp", 1, 0, "RoomsModel");
+    qmlRegisterType<AlbumsModel>("NosonApp", 1, 0, "AlbumsModel");
+    qmlRegisterType<ArtistsModel>("NosonApp", 1, 0, "ArtistsModel");
+    qmlRegisterType<ComposersModel>("NosonApp", 1, 0, "CompisersModel");
+    qmlRegisterType<GenresModel>("NosonApp", 1, 0, "GenresModel");
+    qmlRegisterType<PlaylistsModel>("NosonApp", 1, 0, "PlaylistsModel");
+    qmlRegisterType<TracksModel>("NosonApp", 1, 0, "TracksModel");
+    qmlRegisterType<QueueModel>("NosonApp", 1, 0, "QueueModel");
+    qmlRegisterType<RenderingModel>("NosonApp", 1, 0, "RenderingModel");
+    qmlRegisterType<FavoritesModel>("NosonApp", 1, 0, "FavoritesModel");
+    //qmlRegisterUncreatableType<FavoriteType>("NosonApp", 1, 0, "FavoriteType", "enums");
+    qmlRegisterType<ServicesModel>("NosonApp", 1, 0, "ServicesModel");
+    qmlRegisterType<MediaModel>("NosonApp", 1, 0, "MediaModel");
+    qmlRegisterType<MediaAuth>("NosonApp", 1, 0, "MediaAuth");
+    qmlRegisterType<AlarmsModel>("NosonApp", 1, 0, "AlarmsModel");
 
     // handle signal exit(int) issued by the qml instance
     QObject::connect(&engine, &QQmlApplicationEngine::exit, doExit);
