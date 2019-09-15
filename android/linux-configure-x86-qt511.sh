9132ac8c1eca0b298493b7d76b48cff62c842fa9
janbar <jlbarriere68@gmail.com>
2019-09-15 13:25:53 +0200

    android: configure scripts for linux host

---
+++ android/linux-configure-x86-qt511.sh
@@ -0,0 +1,31 @@
+rm -rf build-x86/*
+mkdir -p build-x86
+cd build-x86
+
+export JAVA_HOME=/home/jlb/bin/jdk1.8.0
+export ANDROID_SDK=/home/shared/Android/Sdk
+export ANDROID_NDK=$ANDROID_SDK/ndk-bundle
+export QT_DIR=/home/shared/Qt/5.11.3/android_x86
+
+cmake ../.. -DCMAKE_SYSTEM_NAME=Android \
+-DCMAKE_PREFIX_PATH=$QT_DIR \
+-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+-DCMAKE_MAKE_PROGRAM=$ANDROID_NDK/prebuilt/linux-x86_64/bin/make \
+-DCMAKE_BUILD_TYPE=Release \
+-DANDROID_ABI="x86" \
+-DANDROID_PLATFORM_LEVEL="android-16" \
+-DQT_ANDROID_TOOLCHAIN_TOOL_PREFIX="i686-linux-android" \
+-DQT_ANDROID_SDK_ROOT=$ANDROID_SDK \
+-DQT_ANDROID_NDK_ROOT=$ANDROID_NDK \
+-DQT_ANDROID_QT_ROOT=$QT_DIR \
+-DQT_ANDROID_SDK_BUILDTOOLS_REVISION="21.0.0" \
+-DQt5Core_DIR=$QT_DIR/lib/cmake/Qt5Core \
+-DQt5Gui_DIR=$QT_DIR/lib/cmake/Qt5Gui \
+-DQt5Qml_DIR=$QT_DIR/lib/cmake/Qt5Qml \
+-DQt5Network_DIR=$QT_DIR/lib/cmake/Qt5Network \
+-DQt5Quick_DIR=$QT_DIR/lib/cmake/Qt5Quick \
+-DQt5QuickControls2_DIR=$QT_DIR/lib/cmake/Qt5QuickControls2 \
+-DQt5Xml_DIR=$QT_DIR/lib/cmake/Qt5Xml \
+-DQt5Svg_DIR=$QT_DIR/lib/cmake/Qt5Svg \
+-DQt5Widgets_DIR=$QT_DIR/lib/cmake/Qt5Widgets \
+$@
