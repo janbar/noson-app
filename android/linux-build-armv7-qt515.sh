3a227a4c59e86a3d07487627d099d43a959143de
janbar <jlbarriere68@gmail.com>
2024-09-22 20:46:35 +0200

    revert build with qt-5.15.15 to qt-5.15.2
    Cannot access to media audio using qt-5.15.15, so revert to 5.15.2.

--- android/linux-build-armv7-qt515.sh
+++ android/linux-build-armv7-qt515.sh
@@ -2,10 +2,10 @@ BUILD_DIR=build-x64
 rm -rf $BUILD_DIR/*
 mkdir -p $BUILD_DIR
 
-export JAVA_HOME=$HOME/bin/java/jdk-17.0.12
+export JAVA_HOME=$HOME/bin/java/jdk1.8.0
 export ANDROID_SDK_ROOT=$HOME/bin/android/sdk
 export ANDROID_NDK=$HOME/bin/android/sdk/ndk/22.1.7171670
-export QT_DIR=$HOME/bin/Qt/5.15.15/android
+export QT_DIR=$HOME/bin/Qt/5.15.2/android
 
 cmake .. -B $BUILD_DIR -DCMAKE_SYSTEM_NAME=Android \
 -DCMAKE_PREFIX_PATH=$QT_DIR \
