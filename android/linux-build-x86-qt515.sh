3223fd76e9c56048b21106c35fa7b9b8881ff516
janbar <jlbarriere68@gmail.com>
2025-05-22 10:08:53 +0200

    upgrade android builder

--- android/linux-build-x86-qt515.sh
+++ android/linux-build-x86-qt515.sh
@@ -2,16 +2,18 @@ BUILD_DIR=build-x86
 rm -rf $BUILD_DIR/*
 mkdir -p $BUILD_DIR
 
-export JAVA_HOME=$HOME/bin/java/jdk1.8.0
+export JAVA_HOME=$HOME/bin/java/jdk-17.0.12
 export ANDROID_SDK_ROOT=$HOME/bin/android/sdk
-export ANDROID_NDK=$HOME/bin/android/sdk/ndk/22.1.7171670
-export QT_DIR=$HOME/bin/Qt/5.15.2/android
+export ANDROID_NDK=$HOME/bin/android/sdk/ndk/26.3.11579264
+export QT_DIR=$HOME/bin/Qt/5.15.16/android
 
 cmake .. -B $BUILD_DIR -DCMAKE_SYSTEM_NAME=Android \
 -DCMAKE_PREFIX_PATH=$QT_DIR \
+-DCMAKE_FIND_ROOT_PATH=${QT_DIR} \
 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
 -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK/prebuilt/linux-x86_64/bin/make \
 -DCMAKE_BUILD_TYPE=Debug \
+-DANDROID_STL=c++_static \
 -DANDROID_ABI="x86" \
 -DANDROID_SDK_MINVER=24 \
 -DANDROID_SDK_TARGET=26 \
@@ -20,18 +22,6 @@ cmake .. -B $BUILD_DIR -DCMAKE_SYSTEM_NAME=Android \
 -DANDROID_SDK_ROOT=$ANDROID_SDK_ROOT \
 -DANDROID_NDK=$ANDROID_NDK \
 -DQT_ANDROID_PLATFORM_LEVEL=31 \
--DQt5_DIR=$QT_DIR/lib/cmake/Qt5 \
--DQt5Core_DIR=$QT_DIR/lib/cmake/Qt5Core \
--DQt5Gui_DIR=$QT_DIR/lib/cmake/Qt5Gui \
--DQt5Qml_DIR=$QT_DIR/lib/cmake/Qt5Qml \
--DQt5Network_DIR=$QT_DIR/lib/cmake/Qt5Network \
--DQt5Quick_DIR=$QT_DIR/lib/cmake/Qt5Quick \
--DQt5QuickControls2_DIR=$QT_DIR/lib/cmake/Qt5QuickControls2 \
--DQt5Xml_DIR=$QT_DIR/lib/cmake/Qt5Xml \
--DQt5Svg_DIR=$QT_DIR/lib/cmake/Qt5Svg \
--DQt5Widgets_DIR=$QT_DIR/lib/cmake/Qt5Widgets \
--DQt5QmlModels_DIR=$QT_DIR/lib/cmake/Qt5QmlModels \
--DQt5AndroidExtras_DIR=$QT_DIR/lib/cmake/Qt5AndroidExtras \
 $@
 
 [ $? -eq 0 ] && cmake --build $BUILD_DIR --parallel 8
